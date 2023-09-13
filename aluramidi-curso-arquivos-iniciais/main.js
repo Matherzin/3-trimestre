e65b14d76fc1f2deee3300c9b3b35b9a4d2496a7
Matherzin <133118549+Matherzin@users.noreply.github.com>
2023-09-13 09:42:59 -0300

    Add files via upload

---
+++ aluramidi-curso-arquivos-iniciais/main.js
@@ -0,0 +1,16 @@
+document.querySelector('.tecla_pom');
+
+function tocaSomPom(){
+    document.querySelector('#som_tecla_pom').play();
+    document.querySelector('.tecla_pom').onclick = tocaSomPom
+}
+function tocaSomClap(){
+
+}
+function tocaSomTim(){
+
+}
+function tocaSomPuff(){
+
+}
+function
